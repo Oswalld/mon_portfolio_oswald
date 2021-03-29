7ed8e75cce4fbf732020f8eff6608e4bb38db020
Oswald <ochartrai@normandiewebschool.fr>
2021-03-29 10:10:22 +0200

    Create burger.js

---
+++ js/burger.js
@@ -0,0 +1,29 @@
+
+const menu = document.querySelector(".menu")
+const ham = document.querySelector(".ham")
+const xIcon = document.querySelector(".xIcon")
+const menuIcon = document.querySelector(".menuIcon")
+
+ham.addEventListener("click", toggleMenu)
+
+function toggleMenu() {
+  if (menu.classList.contains("showMenu")) {
+    menu.classList.remove("showMenu");
+    xIcon.style.display = "none";
+    menuIcon.style.display = "block";
+    ham.style.background ="white"
+  } else {
+    menu.classList.add("showMenu");
+    xIcon.style.display = "block";
+    ham.style.background ="#282828"
+    menuIcon.style.display = "none";
+  }
+}
+
+var menuLinks = document.querySelectorAll(".menuLink")
+
+menuLinks.forEach(
+  function (menuLink) {
+    menuLink.addEventListener("click", toggleMenu)
+  }
+)
